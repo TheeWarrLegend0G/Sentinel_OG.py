ad75c71f9d05f1eea4df383f711a0f24ea0787a3
TheewarrlegendOG <tetragramat0n@icloud.com>
2024-12-31 04:46:15 -0600

    Create core.py

---
+++ core.py
@@ -0,0 +1,6 @@
+from sentinel_og.config import Config
+
+def run_sentinel():
+    print("Cargando configuración...")
+    print(f"Modo Debug: {Config.DEBUG}")
+    print("Sentinel OG está listo para la acción.") 
